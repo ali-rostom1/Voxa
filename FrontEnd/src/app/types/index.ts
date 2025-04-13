6c55296b2ee38c0aac34cb2b94de1e22340787db
ali-rostom1 <ali.rostom220@gmail.com>
2025-04-13 16:40:26 +0100

    add types file for typescript

---
+++ FrontEnd/src/app/types/index.ts
@@ -0,0 +1,34 @@
+import type React from "react"
+import type { LucideIcon } from "lucide-react"
+
+export interface VideoCardProps {
+  title: string
+  channel: string
+  views: string
+  timeAgo: string
+  duration: string
+}
+
+export interface CategoryCardProps {
+  title: string
+  count: string
+  color: string
+  icon: React.ReactNode
+}
+export interface NavItem {
+    icon: LucideIcon;
+    label: string;
+    href: string;
+}
+export interface SidebarItemProps {
+    icon: LucideIcon;
+    label: string;
+    href: string;
+    active: boolean;
+    collapsed: boolean;
+}
+  
+export interface SidebarProps {
+    className?: string;
+}
+

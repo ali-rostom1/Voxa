4e13e727fcb6b36384327315fd223f29f07daef8
ali-rostom1 <ali.rostom220@gmail.com>
2025-04-17 12:08:04 +0100

    add corresponding route for me method

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,7 +11,9 @@ Route::prefix('v1')->group(function(){
     Route::post('/refresh',[AuthController::class,'refresh']);
     Route::middleware('auth:api')->group(function(){
         Route::post('/logout',[AuthController::class,'logout']);
+        Route::get('/me',[AuthController::class,'me']);
         Route::apiResource('/videos',VideoController::class);
+        Route::apiResource('/comments',CommentController::class);
     });
 });
 

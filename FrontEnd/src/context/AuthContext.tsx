1168c8b5e4272ff49fdb22608cee2ad0ea9d23e9
ali-rostom1 <ali.rostom220@gmail.com>
2025-04-21 09:49:55 +0100

    add loading global state

--- FrontEnd/src/context/AuthContext.tsx
+++ FrontEnd/src/context/AuthContext.tsx
@@ -5,6 +5,7 @@ import { UserProfile } from '@/types';
 type AuthContextType = {
     user: UserProfile | null;
     isAuthenticated: boolean;
+    loading: boolean;
     login: (userData: UserProfile) => void;
     logout: () => void;
 };
@@ -13,6 +14,7 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [user, setUser] = useState<UserProfile | null>(null);
+    const [loading, setLoading] = useState(true);
     const isAuthenticated = !!user;
   
     useEffect(() => {
@@ -20,29 +22,35 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       if (savedUser) {
         setUser(JSON.parse(savedUser));
       }
+      
+      setLoading(false);
     }, []);
   
     const login = (userData: UserProfile) => {
+      setLoading(true);
       setUser(userData);
       localStorage.setItem('user', JSON.stringify(userData));
+      setLoading(false);
     };
   
     const logout = () => {
+      setLoading(true);
       setUser(null);    
       localStorage.removeItem('user');
+      setLoading(false);
     };
   
     return (
-      <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      <AuthContext.Provider value={{ user, isAuthenticated, loading, login, logout }}>
         {children}
       </AuthContext.Provider>
     );
-  };
+};
   
-  export const useAuth = () => {
+export const useAuth = () => {
     const context = useContext(AuthContext);
     if (!context) {
       throw new Error('useAuth must be used within an AuthProvider');
     }
     return context;
-  };
+};

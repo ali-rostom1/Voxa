1ac7452957a3597a10b10d349b8fc9b64ea97e65
ali-rostom1 <ali.rostom220@gmail.com>
2025-04-21 09:52:00 +0100

    add date-fns package and fontawesome

--- FrontEnd/next.config.ts
+++ FrontEnd/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "d30vieonhtu0fc.cloudfront.net",
+    ],
+  } 
+
 };
 
 export default nextConfig;

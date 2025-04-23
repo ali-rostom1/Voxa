09d8f9c5d80b464f22eeffc3d78d048235cab1da
ali-rostom1 <ali.rostom220@gmail.com>
2025-04-23 13:39:14 +0100

    add explore page

---
+++ FrontEnd/src/app/explore/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { DefaultLayout } from "@/components/layouts/DefaultLayout"
+import { ExploreMainSection } from "@/components/sections/explore-main-section"
+
+export default function Explore() {
+  return (
+    <DefaultLayout>
+        <ExploreMainSection />
+    </DefaultLayout>
+  )
+}

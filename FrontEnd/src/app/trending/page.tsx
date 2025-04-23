0b953de6993a75f7276c617cb89da20d79665bbb
ali-rostom1 <ali.rostom220@gmail.com>
2025-04-23 13:39:35 +0100

    add trending page

---
+++ FrontEnd/src/app/trending/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { DefaultLayout } from "@/components/layouts/DefaultLayout"
+import { TrendingMainSection } from "@/components/sections/trending-main-section"
+export default function Explore() {
+  return (
+    <DefaultLayout>
+        <TrendingMainSection />
+    </DefaultLayout>
+  )
+}

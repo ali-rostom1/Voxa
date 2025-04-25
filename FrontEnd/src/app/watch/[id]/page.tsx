f96297e147fce445b71b82c6918b96600c508a61
ali-rostom1 <ali.rostom220@gmail.com>
2025-04-25 11:22:38 +0100

    feat: add Watch page with video player and default layout

---
+++ FrontEnd/src/app/watch/[id]/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { DefaultLayout } from "@/components/layouts/DefaultLayout"
+import { ExploreMainSection } from "@/components/sections/explore-main-section"
+import VideoPlayer from "@/components/ui/video-player"
+import { Video } from "lucide-react"
+
+export default function Watch() {
+  return (
+    <DefaultLayout>
+        <div className="mx-auto p-4">
+            <VideoPlayer 
+                videoSrc="https://d30vieonhtu0fc.cloudfront.net/uploads/videos/video_680a18c412017/master.m3u8"
+                poster=""
+                onEnded={() => console.log('Video playback ended')}
+            />
+        </div>
+    </DefaultLayout>
+  )
+}

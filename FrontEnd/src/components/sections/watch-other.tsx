0741dfb34fc93e00711325b9964d7ab121b7334a
ali-rostom1 <ali.rostom220@gmail.com>
2025-04-29 09:46:11 +0100

    feat: add WatchOther component to display a list of videos with fallback message

---
+++ FrontEnd/src/components/sections/watch-other.tsx
@@ -0,0 +1,26 @@
+import { VideoCard } from "@/components/ui/video-card";
+import { Video } from "@/types";
+import { FC } from "react";
+import { HorizontalVideoCard } from "../ui/horizontal-video-card";
+
+interface WatchOtherProps {
+    videos: Video[];
+}
+
+export const WatchOther: FC<WatchOtherProps> = ({videos}) => {
+    return (
+        <div className="flex flex-col items-center w-full h-full">
+            {videos.length > 0 ? (
+                <>
+                    {videos.map((video) => (
+                        <HorizontalVideoCard key={video.id} video={video} />
+                    ))}
+                </>
+            ) : (
+                <div className="flex items-center justify-center h-[70vh]">
+                    <p>No videos found</p>
+                </div>
+            )}
+        </div>
+    );
+    }

26c7624972e36d796d4f2eea6023865cb1363642
ali-rostom1 <ali.rostom220@gmail.com>
2025-04-29 09:45:23 +0100

    feat: add Plyr CSS for enhanced video player styling

--- FrontEnd/src/app/layout.tsx
+++ FrontEnd/src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      <link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css" />
       </head>
       <body className="bg-gray-50">
         <AuthProvider>

33d646c8d25d1b6ffd4d5fdf40f139a86ae49ffc
ali-rostom1 <ali.rostom220@gmail.com>
2025-05-02 14:36:59 +0100

    feat: wrap MainSection in ProtectedRoute and Suspense for improved loading and access control

--- FrontEnd/src/app/page.tsx
+++ FrontEnd/src/app/page.tsx
@@ -5,11 +5,18 @@ import { DefaultLayout } from "@/components/layouts/DefaultLayout"
 import apiClient from "@/lib/apiClient"
 import { Video } from "@/types";
 import { MainSection } from "@/components/sections/main-section";
+import { ProtectedRoute } from "@/components/protected-route";
+import { Suspense } from 'react';
+
 export default function Home() {
 
   return (
-    <DefaultLayout>
-        <MainSection/>
-    </DefaultLayout>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen text-gray-600">Loading...</div>}>
+      <ProtectedRoute isPublic>
+        <DefaultLayout>
+            <MainSection/>
+        </DefaultLayout>
+      </ProtectedRoute>
+    </Suspense>
   );
 }

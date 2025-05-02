5fe64e1aaf392470ed7fe2f8f694c7ebb547b2be
ali-rostom1 <ali.rostom220@gmail.com>
2025-05-02 16:46:14 +0100

    feat: create History page with DefaultLayout component

---
+++ FrontEnd/src/app/history/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { DefaultLayout } from "@/components/layouts/DefaultLayout"
+
+
+export default function History(){
+    return (
+        <DefaultLayout>
+            <></>
+        </DefaultLayout>
+    )
+}

d6a23a44ca39b514d58a8024e9d52e27db1bd223
ali-rostom1 <ali.rostom220@gmail.com>
2025-05-03 12:46:09 +0100

    feat: render HistorySection in History component

--- FrontEnd/src/app/history/page.tsx
+++ FrontEnd/src/app/history/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 import { DefaultLayout } from "@/components/layouts/DefaultLayout"
+import { HistorySection } from "@/components/sections/history-section"
 
 
 export default function History(){
     return (
         <DefaultLayout>
-            <></>
+            <HistorySection/>
         </DefaultLayout>
     )
 }

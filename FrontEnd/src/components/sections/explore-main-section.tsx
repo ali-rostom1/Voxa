24ed57a40e6fd5e110d3c679ed832531c1050796
ali-rostom1 <ali.rostom220@gmail.com>
2025-05-03 12:46:26 +0100

    feat: enhance video data structure and improve loading state handling in ExploreMainSection

--- FrontEnd/src/components/sections/explore-main-section.tsx
+++ FrontEnd/src/components/sections/explore-main-section.tsx
@@ -50,17 +50,21 @@ export const ExploreMainSection: FC = () => {
                     const data = response.data.data.data.map((video: any) => ({
                         id: video.id,
                         title: video.title,
-                        user: video.user.name,
+                        category_name: video.category.name,
+                        description: video.description,
+                        user: { 
+                            id: video.user.id,
+                            name: video.user.name,
+                            email: video.user.email,
+                            pfp_path: video.user.pfp_path,
+                        },
                         thumbnail: video.thumbnail_path,
                         views: video.views_count,
                         uploadTime: video.created_at,
                         duration: video.duration,
-                        channelAvatar: video.user.pfp_path,
                     }));
                     setTotalPages(Math.ceil(response.data.data.total/perPage));
-                    console.log(totalPages)
                     setPage(response.data.data.current_page);
-                    console.log(page);
                     setVideos(data || []);
                 } else {
                     setError("Failed to fetch videos");
@@ -74,15 +78,7 @@ export const ExploreMainSection: FC = () => {
         fetchVideos();
     }
     , [sortValue,selectedCategory,page]);
-    if (isLoading) {
-        return (
-            <section className="my-8 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
-                <div className="flex justify-center items-center h-screen">
-                    <p>Loading...</p>
-                </div>
-            </section>
-        );
-    }
+    
     if (error) {
         return (
             <section className="my-8 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
@@ -92,32 +88,30 @@ export const ExploreMainSection: FC = () => {
             </section>
         );
     }
-    if (videos.length === 0) {
-        return (            
-            <section className="my-8 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
-                <div className="flex items-center justify-between mb-6">
-                <CategoryFilter categories={categories} activeCategory={selectedCategory} onCategoryChange={setSelectedCategory} />
-                <SortingFilter onChange={setSortValue} selected={sortValue} setSelected={setSortValue}/>
-                </div>
-                <div className="flex justify-center items-center h-[70vh]">
-                    <p>No videos found</p>
-                </div>
-            </section>
-        );
-    }
     return (
         <section className="my-8 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto h-full">
             <div className="flex items-center justify-between mb-6">
                 <CategoryFilter categories={categories} activeCategory={selectedCategory} onCategoryChange={setSelectedCategory} />
-
                 <SortingFilter onChange={setSortValue} selected={sortValue} setSelected={setSortValue}/>
                 
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-                {videos.map((video) => (
-                <VideoCard key={video.id} video={video} />
-                ))}
-            </div>
+                {isLoading ? (
+                    <section className="my-8 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
+                    <div className="flex justify-center items-center h-screen">
+                        <p>Loading...</p>
+                    </div>
+                </section>
+                ) : videos.length === 0 ? (
+                    <div className="flex justify-center items-center h-[70vh]">
+                    <p>No videos found</p>
+                </div>) : (
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                        {videos.map((video) => (
+                            <VideoCard key={video.id} video={video} />
+                        ))}
+                    </div>
+                )}
+            
             <Pagination className='mt-5' currentPage={page} onPageChange={setPage} totalPages={totalPages}></Pagination>
         </section>
     );

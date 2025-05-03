3669ea75be9a47493288e0f9dfd57c8a05ac4207
ali-rostom1 <ali.rostom220@gmail.com>
2025-05-03 12:46:32 +0100

    feat: implement HistorySection component layout with header and grid structure

--- FrontEnd/src/components/sections/history-section.tsx
+++ FrontEnd/src/components/sections/history-section.tsx
@@ -1,11 +1,17 @@
+import { Section } from "lucide-react";
+import { SectionHeader } from "./section-header";
 
 
 
 export const HistorySection = () => {
 
     return (
-        <>
-        </>
+        <section className="my-8 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
+            <SectionHeader title="History"/>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                
+            </div>
+        </section>
     );
 
 }

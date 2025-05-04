cb2833359f9c6863fddd0804d3f5ec6b0f8c5bae
ali-rostom1 <ali.rostom220@gmail.com>
2025-05-04 17:40:36 +0100

    feat: add playlists API routes for managing playlists and video interactions

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,18 +3,24 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PlaylistController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReactionController;
 use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function(){
+
+
     Route::post('/register',[AuthController::class,'register']);
     Route::post('/login',[AuthController::class,'login']);
     Route::post('/refresh',[AuthController::class,'refresh']);
+
     Route::apiResource('/videos',VideoController::class)->only(['index']);
     Route::apiResource('/videos',VideoController::class)->only(['show'])->middleware('track.view');
     Route::apiResource('categories',CategoryController::class)->only(['index','show']);
+    Route::apiResource('/playlists',PlaylistController::class)->only(["index",'show']);
+
     Route::get('videos/featured/{perPage}',[VideoController::class,'featuredVideos']);
     Route::get('videos/trending/{perPage}',[VideoController::class,'trendingVideos']);
     Route::post('videos/filter',[VideoController::class,'getVideosFiltered']);
@@ -26,6 +32,7 @@ Route::prefix('v1')->group(function(){
         Route::apiResource('/videos',VideoController::class)->except(['index','show']);
         Route::apiResource('/comments',CommentController::class);
         Route::apiResource('/categories',CategoryController::class)->except(['index','show']);
+        Route::apiResource('/playlists',PlaylistController::class)->except(['index','show']);
 
         // Video reactions
         Route::post('/videos/{video}/like', [ReactionController::class, 'like']);
@@ -36,6 +43,10 @@ Route::prefix('v1')->group(function(){
         Route::get('/videos/{video}/subscribe',[ReactionController::class, 'subscribe']);
         Route::post('/videos/history/me',[VideoController::class, 'getHistory']);
         Route::delete('/videos/history/clear',[VideoController::class, 'clearHistory']);
+
+
+        //playlist interactions
+        Route::post('/playlists/add/{playlistId}/{videoId}',[PlaylistController::class,'addVideo']);
 
 
         Route::put('/profile',[ProfileController::class,'update']);

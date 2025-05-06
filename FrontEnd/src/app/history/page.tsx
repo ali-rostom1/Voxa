9fc9bd4f266fe179ff8dc2b3e8ab1a7bbcced1d7
ali-rostom1 <ali.rostom220@gmail.com>
2025-05-06 13:25:47 +0100

    feat: wrap History component in Suspense and ProtectedRoute for improved loading state and access control

--- FrontEnd/src/app/history/page.tsx
+++ FrontEnd/src/app/history/page.tsx
@@ -1,12 +1,18 @@
 "use client"
 import { DefaultLayout } from "@/components/layouts/DefaultLayout"
+import { ProtectedRoute } from "@/components/protected-route"
 import { HistorySection } from "@/components/sections/history-section"
+import { Suspense } from "react"
 
 
 export default function History(){
     return (
-        <DefaultLayout>
-            <HistorySection/>
-        </DefaultLayout>
+        <Suspense fallback={<div className="flex items-center justify-center h-screen text-gray-600">Loading...</div>}>
+                <ProtectedRoute >
+                    <DefaultLayout>
+                        <HistorySection/>
+                    </DefaultLayout>
+                </ProtectedRoute>
+        </Suspense>
     )
 }

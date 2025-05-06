052b84cbd0a8cb02e1bd395eb1d93e4441b7b05d
ali-rostom1 <ali.rostom220@gmail.com>
2025-05-06 13:26:11 +0100

    feat: wrap Explore component in Suspense and ProtectedRoute for improved loading state and access control

--- FrontEnd/src/app/trending/page.tsx
+++ FrontEnd/src/app/trending/page.tsx
@@ -1,10 +1,16 @@
 "use client"
 import { DefaultLayout } from "@/components/layouts/DefaultLayout"
+import { ProtectedRoute } from "@/components/protected-route"
 import { TrendingMainSection } from "@/components/sections/trending-main-section"
+import { Suspense } from "react"
 export default function Explore() {
   return (
-    <DefaultLayout>
-        <TrendingMainSection />
-    </DefaultLayout>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen text-gray-600">Loading...</div>}>
+            <ProtectedRoute isPublic>
+                <DefaultLayout>
+                    <TrendingMainSection/>
+                </DefaultLayout>
+            </ProtectedRoute>
+    </Suspense>
   )
 }

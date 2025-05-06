bff4307e1c63300773b42fc728b58496e52bc5d7
ali-rostom1 <ali.rostom220@gmail.com>
2025-05-06 13:25:41 +0100

    feat: wrap Explore component in Suspense and ProtectedRoute for improved loading state and access control

--- FrontEnd/src/app/explore/page.tsx
+++ FrontEnd/src/app/explore/page.tsx
@@ -1,11 +1,17 @@
 "use client"
 import { DefaultLayout } from "@/components/layouts/DefaultLayout"
+import { ProtectedRoute } from "@/components/protected-route"
 import { ExploreMainSection } from "@/components/sections/explore-main-section"
+import { Suspense } from "react"
 
 export default function Explore() {
   return (
-    <DefaultLayout>
-        <ExploreMainSection />
-    </DefaultLayout>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen text-gray-600">Loading...</div>}>
+        <ProtectedRoute isPublic>
+          <DefaultLayout>
+              <ExploreMainSection />
+          </DefaultLayout>
+        </ProtectedRoute>
+    </Suspense>
   )
 }
